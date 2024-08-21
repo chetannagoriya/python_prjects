2ad15458184a83edc90d59a77e59654a503f18b3
Chetan Nagoriya <152314423+chetannagoriya@users.noreply.github.com>
2024-08-21 16:48:54 +0530

    Create greater_3.py

---
+++ greater_3.py
@@ -0,0 +1,17 @@
+print("Enter the First value:")
+a=int(input())
+print("Enter the Second value:")
+b=int(input())
+print("Enter the Third value")
+c=int(input())
+
+if a>b:
+    if a>c:
+        print( +a,"is greater")
+    else:
+        print(+c,"is greater")
+else:
+    if b>c:
+        print( +b,"is greater")
+    else:
+        print(+c,"is greater")
