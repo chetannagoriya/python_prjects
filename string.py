afe64ab6bd57912a27803ec20dd066067f9571ad
Chetan Nagoriya <152314423+chetannagoriya@users.noreply.github.com>
2024-08-22 16:38:54 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,13 @@
+# STRINGs
+name= "    hello My Name is, chetan"
+# name1= 123
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.strip())
+print(name.replace("chetan","dhanraj"))
+print(name.find("is"))
+print(str(name))
+print(len(name))
+print(name.startswith("    hello"))
+print(name.endswith("chetan"))
